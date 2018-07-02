98e19e805ccb19ddc61c6741c94b6ca17f45d9d8
MJ <melaniejane.pascual@gmail.com>
2018-07-02 16:42:40 -0700

    clean up layout

--- app/src/main/java/com/example/pascm033/marvelcharacters/MainActivity.java
+++ app/src/main/java/com/example/pascm033/marvelcharacters/MainActivity.java
@@ -14,6 +14,7 @@ import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.TextView;
+import android.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
     EditText mCharacterInput;
