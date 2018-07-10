39c37790d63075c5fbef6383638918dfdb7d67d5
MJ <melaniejane.pascual@gmail.com>
2018-07-09 17:50:34 -0700

    separate view holder

--- app/src/main/java/com/example/pascm033/marvelcharacters/MarvelCharacterAdapter.java
+++ app/src/main/java/com/example/pascm033/marvelcharacters/MarvelCharacterAdapter.java
@@ -28,42 +28,6 @@ public class MarvelCharacterAdapter extends
         this.characterInfoList = characterInfoList;
     }
 
-    /**
-     * View Holder binds data to view
-     */
-    class MarvelCharacterViewHolder extends RecyclerView.ViewHolder {
-        private ImageView marvelPic;
-        private View.OnClickListener characterClickListener = new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                SingleCharacterInformationActivity.start(v.getContext(), mCharacterInfo.getName(), mCharacterInfo.getDescription(), mCharacterInfo.getThumbnail() );
-            }
-        };
-
-        private CharacterInfo mCharacterInfo;
-
-        /**
-         * constructor
-         *
-         * @param itemView
-         */
-        public MarvelCharacterViewHolder(View itemView) {
-            super(itemView);
-            marvelPic = itemView.findViewById(R.id.characterImg);
-            itemView.setOnClickListener(characterClickListener);
-        }
-
-        public void bind(CharacterInfo characterInfo) {
-            if(!(characterInfo.getThumbnail().getPath().equals("http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"))){
-                Picasso.get()
-                        .load(Uri.parse(characterInfo.getThumbnail().getPath() + "." + characterInfo.getThumbnail().getExtension()))
-                        .resize(500, 500)
-                        .into(marvelPic);
-            }
-            mCharacterInfo = characterInfo;
-        }
-    }
-
 
     @NonNull
     @Override
--- app/src/main/java/com/example/pascm033/marvelcharacters/MarvelCharacterViewHolder.java
+++ app/src/main/java/com/example/pascm033/marvelcharacters/MarvelCharacterViewHolder.java
@@ -1,4 +1,44 @@
 package com.example.pascm033.marvelcharacters;
 
-public class MarvelCharacterViewHolder {
+import android.net.Uri;
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.ImageView;
+import com.squareup.picasso.Picasso;
+
+/**
+ * View Holder binds data to view
+ */
+class MarvelCharacterViewHolder extends RecyclerView.ViewHolder {
+    private ImageView marvelPic;
+    private View.OnClickListener characterClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            SingleCharacterInformationActivity.start(v.getContext(), mCharacterInfo.getName(), mCharacterInfo.getDescription(), mCharacterInfo.getThumbnail() );
+        }
+    };
+
+    private CharacterInfo mCharacterInfo;
+
+    /**
+     * constructor
+     *
+     * @param itemView
+     */
+    public MarvelCharacterViewHolder(View itemView) {
+        super(itemView);
+        marvelPic = itemView.findViewById(R.id.characterImg);
+        itemView.setOnClickListener(characterClickListener);
+    }
+
+    public void bind(CharacterInfo characterInfo) {
+        if(!(characterInfo.getThumbnail().getPath().equals("http://i.annihil.us/u/prod/marvel/i/mg/b/40/image_not_available"))){
+            Picasso.get()
+                    .load(Uri.parse(characterInfo.getThumbnail().getPath() + "." + characterInfo.getThumbnail().getExtension()))
+                    .resize(500, 500)
+                    .into(marvelPic);
+        }
+        mCharacterInfo = characterInfo;
+    }
 }
+
